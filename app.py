951620b1514679abfc181fd51e71292f56ec439e
yeoncho <yeoncho@student.42seoul.kr>
2020-12-08 22:33:26 +0900

    d

--- app.py
+++ app.py
@@ -53,7 +53,6 @@ def delete_content():
     db.sam.delete_one({'_id': ObjectId(id_receive)})
     return jsonify({'result': 'success'})
 
-<<<<<<< HEAD
 @app.route('/user/create', methods=['POST'])
 def create():
     id_receive = request.form['id_give']
@@ -67,7 +66,3 @@ def create():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-=======
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
->>>>>>> 7eeebc73f2ecbd07bdcb9e21449cb1ad141f65e9
