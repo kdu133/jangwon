e9c582ff03dacfdd703acd56319a7fa255a5672e
youn <2000kogo@naver.com>
2020-12-08 21:02:23 +0900

    dwddwad

--- app.py
+++ app.py
@@ -53,5 +53,7 @@ def delete_content():
     db.sam.delete_one({'_id': ObjectId(id_receive)})
     return jsonify({'result': 'success'})
 
+def ddddddd
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
